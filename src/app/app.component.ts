70da8bccf0242f6ee56314fedc02ea84f997cc43
Milad <milad_0o0@yahoo.com>
2018-03-10 01:38:57 +0200

    it seems to be fixed (about change the color!)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   value='#700006';
+  value1='#705506';
   footer = getComputedStyle(document.body).getPropertyValue('--violet-1');
+  footer00 = getComputedStyle(document.body).getPropertyValue('--violet-00');
 
   constructor(){
 
   }
 
   ngOnInit(){
-    console.log(this.footer);
     this.footer = this.value;
+    this.footer00 = this.value1;
+
   }
 
   onChange(event:Event){
     this.value = (<HTMLInputElement>event.target).value;
     this.footer = this.value;
+  }
+  onChange1(event:Event){
+    this.value = (<HTMLInputElement>event.target).value;
+    this.footer00 = this.value;
   }
 }
